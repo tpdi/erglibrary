50f8d1d5831b3f2b8311c6699f9a92bd999baa0e
TP Diffenbach <tpd@diffenbach.org>
2014-02-19 09:35:28 -0800

    Moved to library.

---
+++ src/org/diffenbach/android/utils/ViewIdGenerator.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.diffenbach.android.utils;
+
+/**
+ * @author tpd
+ *
+ */
+public interface ViewIdGenerator {
+
+	/**
+	 * Returns the initial id of numberNeeded consecutive increasing Ids.
+	 * The next request will return at least (returned + numberNeeded).
+	 * In particular, consecutive Ids will not span a counter rollover.
+	 * 
+	 * @param numberNeeded the number of Ids to reserve.
+	 * @return the first Id of numberNeeded reserved Ids
+	 */
+	int generateViewIds(int numberNeeded);
+
+}
+
