96aa5ff8fa808d5c396d024d6324521fda589892
TP Diffenbach <tpd@diffenbach.org>
2014-02-21 14:07:47 -0800

    Add some comments and setLayoutParams to ViewUtils.

--- src/org/diffenbach/android/utils/ViewUtils.java
+++ src/org/diffenbach/android/utils/ViewUtils.java
@@ -5,22 +5,35 @@ package org.diffenbach.android.utils;
 
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup.LayoutParams;
 
 /**
  * @author TP Diffenbach
  *
  * Utilities that mutate views and return the view, for chaining.
+ * Best used with an import static org.diffenbach.android.utils.*;
  * Because Android doesn't chain setters.
+ * Template methods to return the same derived type as passed.
+ * Views are passed as the second argument, to keep the setting close to
+ * the method name:
+ * 		setLayoutParams(lp, setOrientation(LinearLayout.HORIZONTAL, setId(1, v));
+ * not
+ * 		setLayoutParams(setOrientation(setId(1, v), LinearLayout.HORIZONTAL), lp);
  */
 public class ViewUtils {
 	
-	public static <L extends LinearLayout> L setOrientation( int orientation, L v) {
+	public static <L extends LinearLayout> L setOrientation( final int orientation, final L v) {
 		v.setOrientation(orientation);
 		return v;
 	}
 	
-	public static <V extends View> V setId(int id, V v) {
+	public static <V extends View> V setId(final int id, final V v) {
 		v.setId(id);
+		return v;
+	}
+	
+	public static <V extends View> V setLayoutParams(final LayoutParams lp, final V v) {
+		v.setLayoutParams(lp);
 		return v;
 	}
 
